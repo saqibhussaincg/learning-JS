37ed912efd8371a3af3ed125a0d614290f9dd3f3
Saqib Hussain - Designer + Coder <saqibhussaincg@gmail.com>
2022-03-21 23:19:48 +0500

    'es6'

--- app.js
+++ app.js
@@ -119,7 +119,85 @@ console.log((age >= 18) ? 'you can vote' : 'you cant vote');
 
 // while loop statement 
 
+var num = 0;
+// block scope
+while( num <= 10){
+    console.log(num);
+    num++
+}
+// ek bhi condition false koi output nahi anay wala
+
+// do while loop statement 
+
+var num = 0;
+do{
+    // debugger;
+    console.log(num);
+    num++
+} while ( num <= 10);
+/* 
+    num mai 1 plus karte rakho kab tak ? 
+    jub tak k num less then ya baraber nahi ho jata 10 k 
+
+    is mai condition false ho magar 1 output deta hai kyun k 
+    condition last mai check kar raha hota hai
+*/
+
+// For Loop
+/* 
+         var num = 10;  num < 10   num++
+    for(initializer; condition; iteration)
+    {
+    code to be executed    
+    }
+*/
+
+for(var num = 0; num <= 10; num++){ 
+    console.log(num);
+}
+
+var table = 2;
+for( var num = 1; num <= 10; num++)
+{
+    console.log(table + ' x ' + num + ' = ' + table * num );
+}
 
 
+// 04 Chapter Functions
 
-// djadaklfjafhkldfasdlfhasfasdhfs
+function sum(){
+    var a = 10, b = 20;
+    var total = a + b
+    console.log(total);
+}
+
+sum();
+
+//           parameter
+function sums(a, b){
+    var total = a + b
+    var multi = a * b
+    console.log(total, multi);
+}
+//   argument
+sums(10, 20);
+sums(20, 20);
+
+// Function Expression & Return
+// Function Expression ka mtlb function ko variable mai rakh dena
+
+function one(a, b){
+    return check = a + b;
+}
+
+var functionExpression = one(30, 20)
+
+console.log( functionExpression );
+
+// Return ko call bhi kara lia alag function mai 
+function two(){
+    return check
+}
+console.log(check);
+
+// 05 Chapter ECMA Script ES6
