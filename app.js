35d7278b56d059fb4e919c99c0c792adf24dee13
saqib <saqibhussaincg@gmail.com>
2022-03-23 02:57:10 +0500

    splice

--- app.js
+++ app.js
@@ -277,6 +277,60 @@ os ek index number likh do k is k agay se search karana hai
 */
 
 console.log(myFriends.lastIndexOf('sufiyan'));
+console.log(myFriends.includes('sufiyan')); // true ya false retuen karta hai
+
+const price = [100, 150, 200, 250, 300, 400, 500];
+// find price < 400 less then 400
+// array prototype find
+
+const findPrice = price.find( (item) => {
+    return item < 400;
+})
+console.log(findPrice); // 100
+// find mai problem ye hai k sirf 1 hi element return karta hai 
 
 
+// Array Filter Prototype
+const newPriceTag = price.filter( (item) => {
+    return item < 400;
+})
 
+console.log(newPriceTag);
+// return a new array containing all elements
+// new array bana k liye jis mai 400 se kam price hain
+
+
+// How to Sort And Compare an Array
+// Array Prototype Sort
+
+const months = ['march', 'february', 'january', 'april'];
+console.log(months.sort());
+// sort kar dega a to z
+
+const animals = ['parrot', 'cow', 'sheep'];
+animals.push('chicken') // akhir mai add karega
+// array push new length of array return karta hai
+
+animals.unshift('goat') // shuru mai add karega
+
+// pop last wala remove kar dega
+// shift shuru wala remove kar dega
+
+console.log(animals); 
+
+
+const myMonths = ['jan', 'feb', 'march', 'april'];
+//         from where to add   delete  add
+myMonths.splice(4,              0,    'may'); 
+
+console.log(myMonths);
+
+//           start  delete     update
+myMonths.splice(1,   1,       'Feb')
+
+console.log(myMonths);
+
+/* 
+ 
+
+*/
